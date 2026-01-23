818269c1097712b7e766668d314daf2a0b4f86be
Wafaa Zahir <wzahir@c3r3p2.1337.ma>
2026-01-23 12:55:27 +0100

    add 2fa in front for testing

--- my_project/frontend/src/Setting.tsx
+++ my_project/frontend/src/Setting.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react"
 
 
-function Setting(){
-
-
+export default function Setting()
+{
+    const [twoFactor, setTwoFactor] = useState(false)
+    const [method, setMethod] = useState<"email" | "app" | null>(null)
     return(
         <div>
-            hiii from wafaaa 
+            <h1>Settings</h1>
+            <button onClick={() => setTwoFactor(!twoFactor)}>2FA</button>
+            <p>2FA is : {twoFactor ? "ON" : "OFF"}</p>
+            {twoFactor && (
+            <div>
+                <p>choose 2FA method:</p>
+                <button onClick = {() => setMethod("email")}> Email</button>
+                <button onClick={() => setMethod("app")}>Authenticator App</button>
+            </div>
+            )}
         </div>
     )
 }
 
-export default Setting;

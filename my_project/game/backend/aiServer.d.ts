54f3d2887eda3b390fc25e48eee97589ac21ea55
Wafaa Zahir <wzahir@c3r3p2.1337.ma>
2026-02-12 21:09:04 +0100

    without asma folder

--- my_project/game/backend/aiServer.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=aiServer.d.ts.map
--- my_project/game/backend/server.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const gameRooms: Map<string, {
-    player1: string;
-    player2: string;
-}>;
-//# sourceMappingURL=server.d.ts.map
